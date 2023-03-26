0f14a0fac9a299122859127884c045996a2182c0
ArtemiyGranat <artemiigranat@gmail.com>
2023-03-26 19:37:57 +0400

    added tui, need to add chat support in tui

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,37 +10,41 @@ use client::*;
 
 #[tokio::main]
 async fn main() {
-    let mut socket = match TcpStream::connect("localhost:8080").await {
-        Ok(socket) => socket,
-        Err(_) => {
-            ui::display_error("Could not connect to this server. Try again later.");
-            std::process::exit(1)
-        }
-    };
+    // let mut socket = match TcpStream::connect("localhost:8080").await {
+    //     Ok(socket) => socket,
+    //     Err(_) => {
+    //         // ui::display_error("Could not connect to this server. Try again later.");
+    //         std::process::exit(1)
+    //     }
+    // };
 
-    let (stream_reader, mut writer) = socket.split();
+    // let (stream_reader, mut writer) = socket.split();
 
-    let mut stream_reader = BufReader::new(stream_reader);
-    let mut stdin_reader = BufReader::new(tokio::io::stdin());
+    // let mut stream_reader = BufReader::new(stream_reader);
+    // let mut stdin_reader = BufReader::new(tokio::io::stdin());
 
-    let _username = validate_username(&mut stdin_reader, &mut stream_reader, &mut writer).await;
-    let mut stream_line = String::new();
-    let mut stdin_line = String::new();
-    loop {
-        ui::display_message_waiting();
-        tokio::select! {
-            received_data = stream_reader.read_line(&mut stream_line) => {
-                // TODO: Fix this
-                if received_data.unwrap() == 0 {
-                    break;
-                }
-                ui::display_message(&stream_line);
-                stream_line.clear();
-            }
-            _ = stdin_reader.read_line(&mut stdin_line) => {
-                writer.write_all(&stdin_line.as_bytes()).await.unwrap();
-                stdin_line.clear();
-            }
-        }
-    }
+    // let _username = validate_username(&mut stdin_reader, &mut stream_reader, &mut writer).await;
+    // let mut stream_line = String::new();
+    // let mut stdin_line = String::new();
+    // tokio::spawn(async move {
+        // });
+        
+    ui::run_tui().unwrap();
+    // loop {
+    //     // ui::display_message_waiting();
+    //     tokio::select! {
+    //         received_data = stream_reader.read_line(&mut stream_line) => {
+    //             // TODO: Fix this
+    //             if received_data.unwrap() == 0 {
+    //                 break;
+    //             }
+    //             // ui::display_message(&stream_line);
+    //             stream_line.clear();
+    //         }
+    //         _ = stdin_reader.read_line(&mut stdin_line) => {
+    //             writer.write_all(&stdin_line.as_bytes()).await.unwrap();
+    //             stdin_line.clear();
+    //         }
+    //     }
+    // }
 }
--- client/src/ui.rs
+++ client/src/ui.rs
@@ -1,26 +1,159 @@
-use chrono::Local;
-use std::io::Write;
-use termion::{color, style};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
 
-// TODO: Add the Data struct with username, date and data fields
-// TODO: Color the username with green color
-
-pub fn display_error(error: &str) {
-    eprintln!("{}[ERROR] {}{}", color::Fg(color::Red), style::Reset, error);
+enum InputMode {
+    Normal,
+    Insert,
 }
 
-pub fn display_message(message: &str) {
-    std::io::stdout().flush().unwrap();
-    print!(
-        "\n\r{}[{}] {}{}",
-        color::Fg(color::Blue),
-        Local::now().format("%Y-%m-%d %H:%M:%S").to_string(),
-        style::Reset,
-        message
-    );
+struct App {
+    input: String,
+    input_mode: InputMode,
+    messages: Vec<String>,
 }
 
-pub fn display_message_waiting() {
-    print!("{}[YOU] {}", color::Fg(color::Blue), style::Reset);
-    std::io::stdout().flush().unwrap();
+impl Default for App {
+    fn default() -> App {
+        App {
+            input: String::new(),
+            input_mode: InputMode::Normal,
+            messages: Vec::new(),
+        }
+    }
+}
+
+pub fn run_tui() -> Result<(), Box<dyn Error>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let app = App::default();
+    let res = run_app(&mut terminal, app);
+
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+
+        if let Event::Key(key) = event::read().unwrap() {
+            match app.input_mode {
+                InputMode::Normal => match key.code {
+                    KeyCode::Char('i') => {
+                        app.input_mode = InputMode::Insert;
+                    }
+                    KeyCode::Char('q') => {
+                        return Ok(());
+                    }
+                    _ => {}
+                },
+                InputMode::Insert => match key.code {
+                    KeyCode::Enter => {
+                        app.messages.push(app.input.drain(..).collect());
+                    }
+                    KeyCode::Char(c) => {
+                        app.input.push(c);
+                    }
+                    KeyCode::Backspace => {
+                        app.input.pop();
+                    }
+                    KeyCode::Esc => {
+                        app.input_mode = InputMode::Normal;
+                    }
+                    _ => {}
+                },
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(0)
+        .constraints(
+            [
+                Constraint::Percentage(90),
+                Constraint::Percentage(10),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    let (msg, _) = match app.input_mode {
+        InputMode::Normal => (
+            vec![
+                Span::raw(" Press "),
+                Span::styled("q", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to exit, "),
+                Span::styled("i", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to enter the insert mode"),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        ),
+        InputMode::Insert => (
+            vec![
+                Span::raw(" Press "),
+                Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to enter the normal mode, "),
+                Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to send a message"),
+            ],
+            Style::default(),
+        ),
+    };
+    let messages: Vec<ListItem> = app
+        .messages
+        .iter()
+        .enumerate()
+        .map(|(i, m)| {
+            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+            ListItem::new(content)
+        })
+        .collect();
+    let messages =
+        List::new(messages).block(Block::default().borders(Borders::ALL).title(msg));
+    f.render_widget(messages, chunks[0]);
+
+    let input = Paragraph::new(app.input.as_ref())
+        .style(match app.input_mode {
+            InputMode::Normal => Style::default(),
+            InputMode::Insert => Style::default().fg(Color::Yellow),
+        })
+        .block(Block::default().borders(Borders::ALL).title(" Enter the message"));
+    f.render_widget(input, chunks[1]);
+    match app.input_mode {
+        InputMode::Normal => {}
+        InputMode::Insert => {
+            f.set_cursor(chunks[2].x + app.input.width() as u16 + 1, chunks[2].y + 1)
+        }
+    }
 }
