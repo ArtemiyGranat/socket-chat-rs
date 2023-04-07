c6728a77dd98f82eb8157a509ac5ddd9def02d90
ArtemiyGranat <artemiigranat@gmail.com>
2023-04-07 13:07:05 +0400

    error handling improvements

--- server/src/config.rs
+++ server/src/config.rs
@@ -1,3 +1,6 @@
+use std::net::SocketAddr;
+use crate::Result;
+
 #[derive(Clone)]
 pub struct Config {
     pub server_address: String,
@@ -22,11 +25,19 @@ impl Default for Config {
 }
 
 impl Config {
-    pub fn is_valid_username(&self, username: &str) -> bool {
-        (self.min_username_len..=self.max_username_len).contains(&username.len())
+    pub fn is_valid_username(&self, username: Option<&str>, client_addr: SocketAddr) -> Result<bool> {
+        if let Some(username) = username {
+            Ok((self.min_username_len..=self.max_username_len).contains(&username.len()))
+        } else {
+            Err(format!("Invalid request from {}", client_addr).into())
+        }
     }
 
-    pub fn is_valid_message(&self, message: &str) -> bool {
-        (self.min_message_len..=self.max_message_len).contains(&message.len())
+    pub fn is_valid_message(&self, message: Option<&str>, client_addr: SocketAddr) -> Result<bool> {
+        if let Some(message) = message {
+            Ok((self.min_message_len..=self.max_message_len).contains(&message.len()))
+        } else {
+            Err(format!("Invalid request from {}", client_addr).into())
+        }
     }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,9 +1,9 @@
 use crate::config::Config;
 use crate::Result;
 use crate::{conn_message, disc_message, request_to_json, response_to_json};
-use chrono::{Local, Utc};
+use chrono::Utc;
 use log::info;
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::net::SocketAddr;
 use tokio::io::AsyncWrite;
 use tokio::{
@@ -57,20 +57,20 @@ async fn handle_client(
     // TODO: Send connection message to all other clients (_conn_message should be used).
     handle_new_connection(username.clone());
 
-    let mut data = String::new();
+    let mut request = String::new();
     loop {
         tokio::select! {
-            received_data_size = reader.read_line(&mut data) => {
-                if let Ok(0) = received_data_size {
+            request_size = reader.read_line(&mut request) => {
+                if let Ok(0) = request_size {
                     handle_disconnection(username, sender, client_addr);
                     break Ok(())
                 }
-                handle_received_data(
+                handle_request(
                     config,
-                    received_data_size,
+                    request_size,
                     client_addr,
                     username.clone(),
-                    &mut data,
+                    &mut request,
                     sender,
                     &mut writer
                 )
@@ -80,10 +80,10 @@ async fn handle_client(
                 let (message, sender_addr) = outgoing_data.unwrap();
                 if let Some(sender_addr) = sender_addr {
                     if client_addr != sender_addr {
-                        writer.write_all(message.as_bytes()).await.unwrap();
+                        write_data(&mut writer, message).await?;
                     }
                 } else {
-                    writer.write_all(message.as_bytes()).await.unwrap();
+                    write_data(&mut writer, message).await?;
                 }
             }
         }
@@ -91,42 +91,38 @@ async fn handle_client(
 }
 
 // TODO: Add request handling for the future
-async fn handle_received_data<W: AsyncWrite + Unpin>(
+async fn handle_request<W: AsyncWrite + Unpin>(
     config: &Config,
     size: std::io::Result<usize>,
     client_addr: SocketAddr,
     username: String,
-    data: &mut String,
+    request: &mut String,
     sender: &mut Sender<(String, Option<SocketAddr>)>,
     writer: &mut W,
 ) -> Result<()> {
     if let Err(e) = size {
         return Err(e.into());
     }
-    let json_data: Value = serde_json::from_str(data).unwrap();
-    let msg = json_data
-        .get("body")
-        .and_then(|v| v.as_str())
-        .unwrap()
-        .to_string();
-    if config.is_valid_message(msg.trim()) {
+    let json_request: Value = serde_json::from_str(request).unwrap();
+    let message = json_request.get("body").and_then(|v| v.as_str());
+
+    if config.is_valid_message(message, client_addr)? {
         info!("{} sent a message to the server", username);
-        let now = Local::now().format("%Y-%m-%d %H:%M:%S %z").to_string();
+        let now = Utc::now().format("%Y-%m-%d %H:%M:%S %z").to_string();
         let request = request_to_json!(
             "SendMessage",
-            serde_json::json!({ "data": msg.trim(), "sender": username, "date": now })
+            json!({ "data": message.unwrap().to_string().trim(), "sender": username, "date": now })
         );
         sender.send((request, Some(client_addr))).unwrap();
     } else {
         let response = response_to_json!(400, "InvalidMessage");
-        writer.write_all(response.as_bytes()).await.unwrap();
+        write_data(writer, response).await?;
     }
 
-    data.clear();
+    request.clear();
     Ok(())
 }
 
-// TODO: Fix the logic 
 async fn validate_username<W: AsyncWrite + Unpin>(
     config: &Config,
     reader: &mut BufReader<ReadHalf<'_>>,
@@ -135,36 +131,29 @@ async fn validate_username<W: AsyncWrite + Unpin>(
 ) -> Result<String> {
     loop {
         let mut request = String::new();
-        let mut username = String::new();
+        let mut username: Option<&str> = None;
 
-        // TODO: Fix the issue - panic on client disconnection during the login procedure
-        reader.read_line(&mut request).await.unwrap();
-        // if (reader.read_line(&mut request).await).is_err() {
-            // return Err(format!("{} disconnected before entering username", client_addr).into());
-        // }
-        // Need to get rid of unwrap and add the empty request/invalid request check
-        let json_data: Value = serde_json::from_str(&request).unwrap();
-        let (response, status_code) =
-            if let Some("LogInUsername") = json_data.get("method").and_then(|v| v.as_str()) {
-                username = json_data
-                    .get("body")
-                    .and_then(|v| v.as_str())
-                    .unwrap()
-                    .to_string();
-                if config.is_valid_username(&username) {
+        reader.read_line(&mut request).await?;
+        if request.is_empty() {
+            return Err(format!("{} disconnected before entering username", client_addr).into());
+        }
+        let json_request: Value = serde_json::from_str(&request).unwrap();
+
+        let (response, status_code) = match json_request.get("method").and_then(|v| v.as_str()) {
+            Some("LogInUsername") => {
+                username = json_request.get("body").and_then(|v| v.as_str());
+                if config.is_valid_username(username, client_addr)? {
                     (response_to_json!(200, "OK"), 200)
                 } else {
                     (response_to_json!(400, "InvalidUsername"), 400)
                 }
-            } else {
-                (response_to_json!(400, "BadRequest"), 400)
-            };
-        // TODO: Add send_data function to avoid code duplication
-        if (writer.write_all(response.as_bytes()).await).is_err() {
-            return Err(format!("{} disconnected before entering username", client_addr).into());
-        }
+            }
+            _ => (response_to_json!(400, "BadRequest"), 400),
+        };
+
+        write_data(writer, response).await?;
         if status_code == 200 {
-            return Ok(username);
+            return Ok(username.unwrap().to_string());
         }
     }
 }
@@ -184,9 +173,11 @@ fn handle_disconnection(
         username, client_addr
     );
     let now = Utc::now().format("%Y-%m-%d %H:%M:%S %z").to_string();
-    let request = request_to_json!(
-        "Connection",
-        serde_json::json!({ "data": disc_message, "date": now })
-    );
+    let request = request_to_json!("Connection", json!({ "data": disc_message, "date": now }));
     sender.send((request, Some(client_addr))).unwrap();
+}
+
+async fn write_data<W: AsyncWrite + Unpin>(writer: &mut W, data: String) -> Result<()> {
+    writer.write_all(data.as_bytes()).await?;
+    Ok(())
 }
